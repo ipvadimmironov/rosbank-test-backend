c41a2243d487e3b95643839531b317f187b4b54f
Vadim Mironov <vmironov1@gmail.com>
2024-03-15 10:45:47 +0300

    Added list of possible endpoints to README

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,22 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.getHttpAdapter().getInstance().set('json spaces', 2);
   await app.listen(3005);
+  const server = app.getHttpServer();
+  const router = server._events.request._router;
+
+  const availableRoutes: [] = router.stack
+    .map(layer => {
+      if (layer.route) {
+        return {
+          route: {
+            path: layer.route?.path,
+            method: layer.route?.stack[0].method,
+          },
+        };
+      }
+    })
+    .filter(item => item !== undefined);
+  console.log(availableRoutes);
+
 }
 bootstrap();
